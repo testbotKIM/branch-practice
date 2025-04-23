398b5dd4734d0c4d6d9fbc60c5c9745a9827b4eb
testbotKIM <testbotkim@gmail.com>
2025-04-23 11:14:37 +0900

    refactor: fizzbuzz with 1 if Statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
-for i in range(1,17+1):
-    if i % 15 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
+for i in range(1,19+1):
+    if i % 3 == 0 or i % 5 == 0:
+        print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
         print(f'{i}')
