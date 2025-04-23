b76e4fb5445571e475dca9bdafbce560c1d64e51
testbotKIM <testbotkim@gmail.com>
2025-04-23 11:29:03 +0900

    feat: Adjust endnum to 20

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,5 @@
 # if문 1개로 fizzbuzz 구현하기
-for i in range(1,5+1):
->>>>>>> 02f7d48 (docs: Add commentary)
+for i in range(1,20+1):
     if i % 3 == 0 or i % 5 == 0:
         print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
