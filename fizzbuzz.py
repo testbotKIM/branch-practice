49264ef5789f903503f815d585d1be54e84c23cb
testbotKIM <testbotkim@gmail.com>
2025-04-23 11:27:37 +0900

    docs: Add commentary

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,6 @@
+# if문 1개로 fizzbuzz 구현하기
 for i in range(1,5+1):
+>>>>>>> 02f7d48 (docs: Add commentary)
     if i % 3 == 0 or i % 5 == 0:
         print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
